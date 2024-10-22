468cf2627520936559202b43451ee67c76c3775e
Nathan McCormack <G00414780@atu.ie>
2024-10-22 11:06:43 +0100

    Added PUT and DELETE methods along with validation

--- src/main/java/ie/atu/week6/Product.java
+++ src/main/java/ie/atu/week6/Product.java
@@ -1,16 +1,25 @@
 package ie.atu.week6;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
 
-    private int id;
-    private double price;
+    @NotBlank(message = "ID cannot be blank")
+    private String id;
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Price cannot be blank")
+    @Min(value = 0, message = "Price cannot be 0")
+    private double price;
 
 }
--- src/main/java/ie/atu/week6/ProductController.java
+++ src/main/java/ie/atu/week6/ProductController.java
@@ -1,9 +1,7 @@
 package ie.atu.week6;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +9,48 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private ProductService myService;
+
+    public ProductController(ProductService myService) {
+        this.myService = myService;
+    }
 
     private List<Product> list = new ArrayList<>();
     //Creating a class which is interested in requests and responses. Seperation of Concern
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return list;
+    }
+
     @PostMapping
     public List<Product> newProduct(@RequestBody Product product){
 
         //Send it to do business logic
-        ProductService myService = new ProductService();
         list = myService.addProduct(product);
         return list;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<List> updateProduct(@PathVariable String id, @RequestBody Product updatedProduct){
+
+        for(Product product : list){
+            if(product.getId().equals(id)){
+                list.remove(product);
+            }
+        }
+        list.add(updatedProduct);
+        return ResponseEntity.ok(list);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List> deleteProduct(@PathVariable String id){
+
+        for (Product product : list){
+            if (product.getId().equals(id)){
+                list.remove(product);
+            }
+        }
+        return ResponseEntity.ok(list);
     }
 }
